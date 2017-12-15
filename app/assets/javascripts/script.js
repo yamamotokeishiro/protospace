d9cab27d09a588e6eb1498678afd156561485fe1
saya-lis <itosayaka@itousayakanoMacBook-puro.local>
2017-12-15 13:33:47 +0900

    fixed: script.js 短くまとめる

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,48 +1,13 @@
 $(function(){
-    $('input#prototype_captured_images_attributes_0_content').change(function(){
+    $('input').change(function(){
         var file = $(this).prop('files')[0];
+        var image_find = $(this).parent().find('img');
         if(!file.type.match('image.*')){
             return;
         }
         var fileReader = new FileReader();
         fileReader.onloadend = function() {
-            $('#main_image_uploader > img').attr({src: fileReader.result});
-        }
-        fileReader.readAsDataURL(file);
-    });
-
-    $('input#prototype_captured_images_attributes_1_content').change(function(){
-        var file = $(this).prop('files')[0];
-        if(!file.type.match('image.*')){
-            return;
-        }
-        var fileReader = new FileReader();
-        fileReader.onloadend = function() {
-            $('.0 > img').attr({src: fileReader.result});
-        }
-        fileReader.readAsDataURL(file);
-    });
-
-    $('input#prototype_captured_images_attributes_2_content').change(function(){
-        var file = $(this).prop('files')[0];
-        if(!file.type.match('image.*')){
-            return;
-        }
-        var fileReader = new FileReader();
-        fileReader.onloadend = function() {
-            $('.1 > img').attr({src: fileReader.result});
-        }
-        fileReader.readAsDataURL(file);
-    });
-
-    $('input#prototype_captured_images_attributes_3_content').change(function(){
-        var file = $(this).prop('files')[0];
-        if(!file.type.match('image.*')){
-            return;
-        }
-        var fileReader = new FileReader();
-        fileReader.onloadend = function() {
-            $('.2 > img').attr({src: fileReader.result});
+            image_find.attr({src: fileReader.result});
         }
         fileReader.readAsDataURL(file);
     });
