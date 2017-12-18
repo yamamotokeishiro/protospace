0ec12e2d15630cdee4505cc5ee10e2dad5014c23
saya-lis <itosayaka@itousayakanoMacBook-puro.local>
2017-12-19 08:47:08 +0900

    added: routing edit

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
 
-  resources :prototypes, only: [:index, :new, :create, :show, :destroy]
+  resources :prototypes, only: [:index, :new, :create, :show, :destroy, :edit]
   resources :users, only: [:show, :edit, :update]
 end
