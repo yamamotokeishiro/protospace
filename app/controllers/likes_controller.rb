df19688933064141ca2b34e27233da76f8d27ac6
saya-lis <itosayaka@itousayakanoMacBook-puro.local>
2017-12-20 16:10:24 +0900

    added: action to likes_controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,14 @@
 class LikesController < ApplicationController
+  def create
+    @like = Like.create(user_id: current_user.id, prototype_id: params[:prototype_id])
+    @likes = Like.where(prototype_id: params[:prototype_id])
+    @prototypes = Prototype.all
+  end
+
+  def destroy
+    like = Like.find_by(user_id: current_user.id, prototype_id: params[:prototype_id])
+    like.destroy
+    @likes = Like.where(prototype_id: params[:prototype_id])
+    @prototypes = Prototype.all
+  end
 end
