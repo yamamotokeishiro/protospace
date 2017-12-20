8f99003416737e9b20ac35e51c91cc14f596e0a9
saya-lis <itosayaka@itousayakanoMacBook-puro.local>
2017-12-20 16:04:49 +0900

    added: likes routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
 
-  resources :prototypes, only: [:index, :new, :create, :show, :destroy]
+  resources :prototypes, only: [:index, :new, :create, :show, :destroy] do
+    resources :likes, only: [:create, :destroy]
+  end
   resources :users, only: [:show, :edit, :update]
 end
