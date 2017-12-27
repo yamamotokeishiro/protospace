c6d1232fc17ec50e72a1afc5f4f2c8795ad729ba
saya-lis <itosayaka@itousayakanoMacBook-puro.local>
2017-12-27 18:03:26 +0900

    edit controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,13 +2,15 @@ class LikesController < ApplicationController
   def create
     @like = Like.create(user_id: current_user.id, prototype_id: params[:prototype_id])
     @likes = Like.where(prototype_id: params[:prototype_id])
-    @prototypes = Prototype.all
+    @prototype = Prototype.all
+    redirect_to prototype_path(params[:prototype_id])
   end
 
   def destroy
     like = Like.find_by(user_id: current_user.id, prototype_id: params[:prototype_id])
     like.destroy
     @likes = Like.where(prototype_id: params[:prototype_id])
-    @prototypes = Prototype.all
+    @prototype = Prototype.all
+    redirect_to prototype_path(params[:prototype_id])
   end
 end
--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -21,6 +21,7 @@ class PrototypesController < ApplicationController
 
   def show
     @prototype = Prototype.find(params[:id])
+    @likes = Like.where(prototype_id: params[:id])
   end
 
   def destroy
