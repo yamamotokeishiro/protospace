2f5759212e1acff3e27997e04e49b0d9f7efc04a
saya-lis <itosayaka@itousayakanoMacBook-puro.local>
2018-01-01 16:45:05 +0900

    controllerを修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -2,7 +2,7 @@ class PrototypesController < ApplicationController
   before_action :set_prototype, only: :show
 
   def index
-    @prototypes = Prototype.all
+    @prototypes = Prototype.order("created_at ASC").page(params[:page]).per(4)
   end
 
   def new
